36dba09bb6cc7a261ba8d1e55610bc14fe060a70
Grzegorz Gruszecki <yogi@cybersport.pl>
2011-10-25 13:02:51 +0200

    Zadanie 7

---
+++ pierwiastek.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+double x,y;
+
+int main(){
+
+    printf("Podaj liczbę rzeczywistą: ");
+    scanf("%lf" , &x);
+
+y = x * x;
+
+    if (x<0) {
+    printf("Podałeś liczbę ujemną...");
+    }  else {
+    printf("Pierwiastek kwadratowy z %lf to %lf\n", x, y);
+    }
+return 0;
+}
